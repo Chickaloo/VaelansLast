c8cf02e859f5ce68c79e0ea70959e61db7123e7d
john otters <johnotters@yahoo.com>
2019-05-03 17:29:34 -0700

    Added camera controls

---
+++ Camera.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+
+var CurrentMouseCoords
+var LastMouseCoords
+
+func _ready():
+	LastMouseCoords = get_global_mouse_position()
+		
+func _process(delta: float) -> void:
+	CurrentMouseCoords = get_global_mouse_position()
+	if Input.is_mouse_button_pressed(BUTTON_RIGHT):
+		var diff = LastMouseCoords - CurrentMouseCoords
+		translate(diff * 5)
+	LastMouseCoords = CurrentMouseCoords
